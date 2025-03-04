d40471a3b97c8a0069bd27683e283798acfd059c
rulkimi <azrulkimi00@gmail.com>
2025-03-04 20:42:33 +0800

    make hero section responsive

--- client/src/components/home/hero-section.vue
+++ client/src/components/home/hero-section.vue
@@ -3,15 +3,15 @@ import Button from '../button.vue'
 </script>
 
 <template>
-  <section class="flex items-center gap-2 my-24">
-    <div class="space-y-6">
+  <section class="flex flex-col md:flex-row items-center gap-12 md:my-24">
+    <div class="space-y-6 md:w-1/2">
       <div class="space-y-2">
         <h1 class="text-5xl font-semibold">Find Perfect Recipes With What You Have</h1>
         <span class="block max-w-lg">Discover delicious meal ideas using ingredients you already have in your kitchen.</span>
       </div>
       <Button size="lg" icon="magnifying-glass">Get Started</Button>
     </div>
-    <div class="w-1/2">
+    <div class="md:w-1/2">
       <img
         class="object-cover rounded-3xl"
         src="../../assets/hero-image.webp"
