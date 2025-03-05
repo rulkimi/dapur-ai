4166ce39e407acc6f54153683c84ed67128b1876
rulkimi <azrulkimi00@gmail.com>
2025-03-05 23:14:22 +0800

    install vite pwa plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue()],
+  plugins: [
+    VueRouter(), 
+    vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
 })
