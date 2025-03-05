76ba182640287ea30d0d97e39d77cc2df59aeecd
rulkimi <azrulkimi00@gmail.com>
2025-03-05 10:08:31 +0800

    after login, go to preferences

--- client/src/pages/auth.vue
+++ client/src/pages/auth.vue
@@ -8,6 +8,11 @@ const router = useRouter()
 
 const email = ref<string>('')
 const password = ref<string>('')
+
+const logIn = () => {
+  console.log('login')
+  router.push('/preferences')
+}
 </script>
 
 <template>
@@ -17,39 +22,42 @@ const password = ref<string>('')
       <span class="block text-3xl font-semibold">DapurAI</span>
       <span class="block text-slate-500">Cari resipi yang sedap dengan bahan-bahan yang anda ada di dapur</span>
     </div>
-    <form class="space-y-2" @submit.prevent>
-      <FormInput
-        id="email"
-        v-model="email"
-        icon="envelope"
-        placeholder="johndoe@mail.com"
-        label="Email"
-      />
-      <FormInput
-        id="password"
-        type="password"
-        v-model="password"
-        icon="lock"
-        placeholder="・・・・・・・"
-        label="Password"
-      />
+    <form class="space-y-4" @submit.prevent="logIn">
+      <div class="space-y-2">
+        <FormInput
+          id="email"
+          v-model="email"
+          icon="envelope"
+          placeholder="johndoe@mail.com"
+          label="Email"
+        />
+        <FormInput
+          id="password"
+          type="password"
+          v-model="password"
+          icon="lock"
+          placeholder="・・・・・・・"
+          label="Password"
+        />
+      </div>
+      <div class="space-y-2">
+        <Button
+          width="full"
+          icon="arrow-right-to-bracket"
+          type="submit"
+        >
+          Log masuk
+        </Button>
+        <Button
+          width="full"
+          variant="primary-outline"
+          icon="home"
+          @click="router.push('/')"
+        >
+          Back Home
+        </Button>
+      </div>
     </form>
-    <div class="space-y-2">
-      <Button
-        width="full"
-        icon="arrow-right-to-bracket"
-      >
-        Log masuk
-      </Button>
-      <Button
-        width="full"
-        variant="primary-outline"
-        icon="home"
-        @click="router.push('/')"
-      >
-        Back Home
-      </Button>
-    </div>
     <p class="block text-slate-500 text-center">
       Tiada akaun? <a href="" class="text-teal-500 hover:underline">Daftar sekarang</a>
     </p>
