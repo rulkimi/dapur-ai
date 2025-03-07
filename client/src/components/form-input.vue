4ad85e9faf9c54b0153bad7d8bdba7bb341c8fad
rulkimi <azrulkimi00@gmail.com>
2025-03-07 12:01:57 +0800

    fix peer icon bug

--- client/src/components/form-input.vue
+++ client/src/components/form-input.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useSlots } from 'vue'
+
 interface FormInputProps {
   modelValue: string
   id: string
@@ -21,6 +23,9 @@ withDefaults(defineProps<FormInputProps>(), {
 })
 
 const emit = defineEmits(['update:modelValue'])
+const slots = useSlots()
+const hasAppendIcon = !!slots['append-icon']
+const hasPrependIcon = !!slots['prepend-icon']
 
 const inputSize = {
   'sm': 'px-2 py-1 h-[28px]',
@@ -44,27 +49,7 @@ const handleInput = (event: Event) => {
     <label :for="id" class="text-slate-500 text-sm">
       {{ label }} <sup v-if="required" class="text-red-500 font-semibold">*</sup>
     </label>
-    <div class="relative">
-      <div class="absolute left-2.5 top-2.5 flex justify-center items-center text-slate-500">
-        <slot name="prepend-icon">
-          <font-awesome-icon
-            v-if="icon && iconPosition === 'prepend'"
-            class="transition-colors duration-300"
-            :class="error ? '!text-red-500' : 'peer-focus:!text-teal-500'"
-            :icon="['fas', icon]"
-          />
-        </slot>
-      </div>
-      <div class="absolute right-2.5 top-2.5 flex justify-center items-center text-slate-500">
-        <slot name="append-icon">
-          <font-awesome-icon
-            v-if="icon && iconPosition === 'append'"
-            class="transition-colors duration-300"
-            :class="error ? '!text-red-500' : 'peer-focus:!text-teal-500'"
-            :icon="['fas', icon]"
-          />
-        </slot>
-      </div>
+    <div class="relative peer">
       <input
         :id="readonly ? id : `${id}-readonly`"
         class="w-full peer transition-colors duration-300"
@@ -79,6 +64,30 @@ const handleInput = (event: Event) => {
         :placeholder="placeholder"
         @input="handleInput"
       >
+      <div
+        v-if="(icon && iconPosition === 'prepend') || hasPrependIcon"
+        class="absolute left-2.5 top-2.5 flex justify-center items-center"
+        :class="error ? '!text-red-500' : 'peer-focus:!text-teal-500 text-slate-500'"
+      >
+        <slot name="prepend-icon">
+          <font-awesome-icon
+            class="transition-colors duration-300"
+            :icon="['fas', icon]"
+          />
+        </slot>
+      </div>
+      <div
+        v-if="(icon && iconPosition === 'append') || hasAppendIcon"
+        class="absolute right-2.5 top-2.5 flex justify-center items-center"
+        :class="error ? '!text-red-500' : 'peer-focus:!text-teal-500 text-slate-500'"
+      >
+        <slot name="append-icon">
+          <font-awesome-icon
+            class="transition-colors duration-300"
+            :icon="['fas', icon]"
+          />
+        </slot>
+      </div>
     </div>
     <p v-if="error && errorMessage" class="text-sm text-red-500">{{ errorMessage }}</p>
   </div>
